95f91c445c38ad447734f87d06f7bb8136b70fba
Ariel Ben Horesh <arielbh@gmail.com>
2014-03-13 16:46:59 +0200

    Group now support INPC

--- Portable/SuiteValue.UI.MVVM/Model/Group.cs
+++ Portable/SuiteValue.UI.MVVM/Model/Group.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace SuiteValue.UI.MVVM.Model
 {
-    public class Group<T> : List<T>
+    public class Group<T> : List<T>, INotifyPropertyChanged
     {
         public Group(string title, IEnumerable<T> items)
             : base(items)
         {
             this.Title = title;
         }
-        public string Title { get; set; }
+
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
